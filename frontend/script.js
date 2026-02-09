3ae4b719ca024579f249268b0e437b2615afe9cc
mohamad AL-Rawashdeh <mohrawashdeh02@gmail.com>
2026-02-09 12:40:25 +0300

    Add login page and authentication logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,4 @@
+// ===== Register Logic =====
 const registerForm = document.getElementById("registerForm");
 
 if (registerForm) {
@@ -20,6 +21,30 @@ if (registerForm) {
 
         alert("Registered successfully");
         window.location.href = "login.html";
+    });
+}
+
+// ===== Login Logic =====
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const email = document.getElementById("login_email").value;
+        const password = document.getElementById("login_password").value;
+
+        const validUsers = [
+            "user1@wewebit.com",
+            "user2@wewebit.com",
+            "user3@wewebit.com"
+        ];
+
+        if (validUsers.includes(email) && password === "12345678") {
+            alert("Login successfully");
+        } else {
+            alert("Invalid email or password");
+        }
     });
 }
 
